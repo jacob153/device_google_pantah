e094cca6c663cec459c869624faa8c1d47038273
mikeNG <mkbestas@lineageos.org>
2022-11-26 15:30:37 -0500

    Add privapp permissions for HbmSVManager Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33

--- device-derp.mk
+++ device-derp.mk
@@ -1,6 +1,10 @@
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/pantah-kernel/Image.lz4
 
+# HBM
+PRODUCT_COPY_FILES += \
+    device/google/pantah/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+    
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor:64 \
