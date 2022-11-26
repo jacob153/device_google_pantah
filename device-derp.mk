1f2d7a2b2918bf7e3101872240a75c2ab71db26b
mikeNG <mkbestas@lineageos.org>
2022-11-26 15:40:29 -0500

    Extract EUICC permissions from stock /system_ext Change-Id: Ibcc9db9f219b005c787cee68042ffc1ca51c02d8

--- device-derp.mk
+++ device-derp.mk
@@ -8,8 +8,7 @@ PRODUCT_COPY_FILES += \
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
-    device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
-    device/google/pantah/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
+device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
         
 # Bluetooth
 PRODUCT_PACKAGES += \
