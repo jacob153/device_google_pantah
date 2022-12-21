525f0894c7cd0e4bd1f682791ed497974a48aa10
hurtcopain <jacksummers715@gmail.com>
2022-12-20 23:59:03 -0500

    Derpfest pantah december(dismemberment)

--- derp_cheetah.mk
+++ derp_cheetah.mk
@@ -2,19 +2,21 @@
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common StatiX stuff.
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
 
-# Parts
-$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
-$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
+include device/google/pantah/device-derp.mk
+
+DERP_BUILDTYPE := Official
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_USES_BLUR := true
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := statix_cheetah
+PRODUCT_NAME := derp_cheetah
 PRODUCT_MODEL := Pixel 7 Pro
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
@@ -24,3 +26,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="cheetah-user 13 TQ1A.221205.011 9244662 release-keys"
 
 BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
+
+$(call inherit-product, vendor/google_devices/cheetah/cheetah.mk)
--- derp_panther.mk
+++ derp_panther.mk
@@ -2,19 +2,21 @@
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common StatiX stuff.
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
 
-# Parts
-$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
-$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
+include device/google/pantah/device-derp.mk
+
+DERP_BUILDTYPE := Official
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_USES_BLUR := true
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := statix_panther
+PRODUCT_NAME := derp_panther
 PRODUCT_MODEL := Pixel 7
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
@@ -24,3 +26,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="panther-user 13 TQ1A.221205.011 9244662 release-keys"
 
 BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
+
+$(call inherit-product, vendor/google_devices/panther/panther.mk)
